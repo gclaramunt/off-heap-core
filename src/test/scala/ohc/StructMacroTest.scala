1d4047e94faad47e6f5a30e7531179cf5855371e
rodrigo <ioniviil@gmail.com>
2015-10-31 01:47:47 -0300

    greatly improved the macro resilience

--- src/test/scala/ohc/StructMacroTest.scala
+++ src/test/scala/ohc/StructMacroTest.scala
@@ -2,14 +2,24 @@ package ohc
 
 import ohc.annotation.struct
 
-trait GenericVector[T] extends Any {
-  def _1: T
-  def _2: T
-
-  def asTuple = (_1, _2)
-}
 object StructMacroTest {
 
+  trait GenericVector[T, A <: Allocator[A]] extends Any {
+    def _1(implicit a: A): T
+    def _2(implicit a: A): T
 
-  @struct(debug = true) class Point[A <: Allocator[A]](var x: Int, var y: Int)
+    def asTuple(implicit a: A) = (_1, _2)
+  }
+  @struct(debug = true) class Point[A <: Allocator[A]](var x: Int, var y: Int) extends GenericVector[Int, A] {
+    def _1(implicit a: A) = x
+    def _2(implicit a: A) = y
+  }
+  object Point {}
+
+
+  val stack = new Stack(new DirectMemory(10))
+  stack.contextualized { implicit c =>
+    val p = Point(4, 65)
+    val Point(a, b) = p
+  }
 }
