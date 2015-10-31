9a45ac430c81a29ba68ce406986f5b584645c566
rodrigo <ioniviil@gmail.com>
2015-10-31 19:56:20 -0200

    Added general purpose cloning

--- src/main/scala/ohc/Struct.scala
+++ src/main/scala/ohc/Struct.scala
@@ -2,8 +2,22 @@ package ohc
 
 import language.higherKinds
 
-trait Struct[A <: Allocator[A]] extends Any
 trait StructDef[T[X <: Allocator[X]] <: Struct[X]] extends Any {
   def apply[A <: Allocator[A]](pointer: Long): T[A]
   def size: Long
+}
+
+trait Struct[A <: Allocator[A]] extends Any {
+  def _ptr: Long
+}
+
+object Struct {
+
+  implicit class StructOps[A <: Allocator[A], S[X <: Allocator[X]] <: Struct[X]](val s: S[A]) extends AnyVal {
+    def cloneIn[B <: Allocator[B]](implicit a: A, b: B, sd: StructDef[S]): S[B] = {
+      val res = b.allocate[S]
+      a.memory.copyTo(b.memory, s._ptr, sd.size, res._ptr)
+      res
+    }
+  }
 }
