df304c8fd795aeb9c584c05d8718506f9493549f
rodrigo <ioniviil@gmail.com>
2015-10-31 19:55:37 -0200

    Added TODO

--- src/main/scala/ohc/Allocator.scala
+++ src/main/scala/ohc/Allocator.scala
@@ -6,6 +6,7 @@ import shapeless.tag._
 
 @implicitNotFound("Can't allocate structure, no memory context available.")
 trait Allocator[Self <: Allocator[Self]] {
-  def allocate[S[X <: Allocator[X]] <: Struct[X]](implicit structDef: StructDef[S]): S[Self]
+  def allocate[S[X <: Allocator[X]] <: Struct[X]](implicit structDef: StructDef[S]): S[Self] //TODO: review this. Scalac currently boxes for an instant during a constructor of a Struct
+                                                                                             // due to escape analysis, it has 0 overhead, but we shouldn't trust escape analysis.
   def memory: Memory
 }
