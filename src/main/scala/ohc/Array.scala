7f7b7afb7f6a1909d6873363e3ed681dd726373b
rodrigo <ioniviil@gmail.com>
2015-11-05 22:42:33 -0200

    Swap to our custom macro based Nats

--- src/main/scala/ohc/Array.scala
+++ src/main/scala/ohc/Array.scala
@@ -1,9 +1,8 @@
 package ohc
 
 import language.higherKinds
-import shapeless.Nat
+import support.Nat._
 import shapeless.tag._
-import shapeless.ops.nat._
 
 class Array[A <: Allocator[A], S[X <: Allocator[X]] <: Struct[X], N <: Nat](val _ptr: Long) extends AnyVal with Struct[A] {
   def apply(m: Nat)(implicit ev: m.N LT N, mDim: ToInt[m.N], sd: StructDef[S]): Long @@ A = (_ptr + sd.size * mDim()).asInstanceOf[Long @@ A]
